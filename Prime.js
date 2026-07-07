a737ca4d56c932adfee3ca8f3b805f4a7cec9cb2
dharsanpriya <p.priyadharsan007@gmail.com>
2026-07-07 09:36:26 +0530

    Update Prime.js

--- Prime.js
+++ Prime.js
@@ -7,19 +7,28 @@
 // const n=parseInt();
 const n=parseInt(process.args[2]);
 function isPrime(n) {
-    if(num<=1)return false;
 
-    for(let i=2;i<num;i++){
-        if (num%i===0){
+const isPrime = (num) => {
+    if (num <= 1) {
+        return false;
+    }
+
+    for (let i = 2; i <= num/2; i++) {
+        if (num % i === 0) {
             return false;
         }
     }
+
     return true;
-}
-if(isPrime(n)){
-    console.log("Prime Number");
-}else{
-    console.log("Not Prime Number");
+};
+
+// Example
+let number = 17;
+
+if (isPrime(number)) {
+    console.log(number + " is a Prime Number");
+} else {
+    console.log(number + " is Not a Prime Number");
 }
 
 module.exports = isPrime;
